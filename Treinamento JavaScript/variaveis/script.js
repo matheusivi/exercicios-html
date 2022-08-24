38144175ff4a14afa53b564061276003ed42526e
Matheus William <matheus-willliam@hotmail.com>
2022-08-24 13:53:15 -0300

    projeto novo mundo invertido html

---
+++ Treinamento JavaScript/variaveis/script.js
@@ -0,0 +1,18 @@
+var nome = ('Matheus')
+var idade = 28; 
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5; 
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco)
+
+var sobrenome = 'Oliveira', cidade = 'MS';
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+console.log(semDefinir);
