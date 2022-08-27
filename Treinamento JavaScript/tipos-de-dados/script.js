633fcd09949267ea9807b8ac6a0c7a539221fc3a
Matheus William <matheus-willliam@hotmail.com>
2022-08-27 00:45:07 -0300

    treinamento com strings

---
+++ Treinamento JavaScript/tipos-de-dados/script.js
@@ -0,0 +1,52 @@
+var nome = 'Matheus'
+var idade = 29;
+var time = null;
+var simbolo = Symbol();
+console.log(typeof simbolo)
+
+var nome ='Matheus'
+var sobrenome='Oliveira'
+var nomeCompleto = nome + ' ' + sobrenome;
+
+console.log(nomeCompleto)
+
+var gols = 1000;
+var frase = 'Romário fez ' + gols + ' gols';
+console.log(frase)
+
+var ano = 2018;
+var mes = 8;
+console.log(ano + mes);
+
+var melhor = 'ótimo'
+var frase1 = "Esse é o " + melhor + " jogo"
+console.log(frase1)
+
+var melhor = 'ótimo'
+var frase1 = "Esse é o  \"melhor\"  jogo"
+console.log(frase1)
+
+var gols = 1000;
+var frase2 = `Romário fez ${gols *3} gols`;
+console.log(frase2)
+
+var pais = 'Jerusalém'
+console.log(typeof pais)
+
+var idade ='29';
+console.log(typeof idade)
+
+var idade = 29;
+console.log(idade);
+
+var nome ='matheus'
+var sobrenome = 'Oliveira'
+var nomeSobrenome = `${nome} ${sobrenome}`
+console.log(nomeSobrenome)
+
+var frase = 'it\'s time'
+console.log(frase)
+
+console.log(typeof nome);
+
+
