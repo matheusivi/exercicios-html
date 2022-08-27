2bc9be3f609ecb6a90179a58de765392c5d816df
Matheus William <matheus-willliam@hotmail.com>
2022-08-26 23:54:29 -0300

    treinamento com variavel

--- Treinamento JavaScript/variaveis/script.js
+++ Treinamento JavaScript/variaveis/script.js
@@ -1,18 +1,49 @@
-var nome = ('Matheus')
-var idade = 28; 
-var possuiFaculdade = true;
-
-console.log(nome, idade, possuiFaculdade);
+var nome = 'Matheus'
+var idade = '29'
+var possuiFaculdade = 'true';
+console.log(possuiFaculdade, nome, idade, nome)
 
 var preco = 25;
-var totalComprado = 5; 
+var totalComprado = 5;
 var totalPreco = totalComprado * preco;
 
 console.log(totalPreco)
 
-var sobrenome = 'Oliveira', cidade = 'MS';
+var sobrenome = 'William', 
+    cidade = 'MS'; 
 
-console.log(sobrenome, cidade);
+console.log(sobrenome, cidade)
 
-var semDefinir;
-console.log(semDefinir);
+var semDefenir;
+console.log(semDefenir);
+
+ var comida
+ console.log(comida);
+ comida = 'Pizza';
+ console.log(comida);
+
+ var time ='Vasco';
+ time = 'flamengo';
+ console.log(time);
+
+
+ var nome ='Matheus'
+ var sobrenome = 'Oliveira'
+
+ console.log(nome, sobrenome);
+
+ var idade = '29'
+ var tipoSanguineo = 'A+'
+
+ console.log(idade, tipoSanguineo);
+
+ var comida = 'pizza';
+ var semDefenir;
+
+ console.log(comida, semDefenir)
+
+ var time, 
+    escola, 
+    cidade, 
+    pais, 
+    curso;
