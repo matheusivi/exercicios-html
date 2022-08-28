b8597deaa330aa12a8811793f28c77c5bc599d3a
Matheus William <matheus-willliam@hotmail.com>
2022-08-28 11:12:41 -0300

    JS, numeros e operadores

---
+++ Treinamento JavaScript/numeros-e-operadores1/script.js
@@ -0,0 +1,39 @@
+var idade = 23;
+console.log(idade);
+
+var tamanho = 2e2;
+console.log(tamanho)
+
+//Operadores aritméticos
+
+var soma= 100 + 50
+console.log(soma)
+
+var subtracao = 100 - 50;
+console.log(subtracao)
+
+var multiplicacao = 100 * 2;
+console.log(multiplicacao)
+
+var expoente = 2 ** 4;
+console.log(expoente);
+
+var modulo = 14 % 5
+console.log(modulo)
+ 
+// String
+
+var soma = '100' + 50
+console.log(soma)
+
+var subtracao = '100' - 50
+console.log(subtracao)
+
+var multiplicacao = '100' * '2'
+console.log(multiplicacao)
+
+var divisao = 'comprei 10' / 2
+console.log(divisao)
+
+var testeNaN = 'isso é 100' / 2
+console.log(isNaN(testeNaN));
