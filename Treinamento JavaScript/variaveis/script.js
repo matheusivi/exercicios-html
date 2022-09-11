57eed27a51e144812ec6051afad8e37a4a56284d
Matheus William <matheus-willliam@hotmail.com>
2022-09-10 21:52:29 -0300

    javaScript começo

--- Treinamento JavaScript/variaveis/script.js
+++ Treinamento JavaScript/variaveis/script.js
@@ -1,49 +1,38 @@
-var nome = 'Matheus'
-var idade = '29'
-var possuiFaculdade = 'true';
-console.log(possuiFaculdade, nome, idade, nome)
-
-var preco = 25;
-var totalComprado = 5;
-var totalPreco = totalComprado * preco;
-
-console.log(totalPreco)
-
-var sobrenome = 'William', 
-    cidade = 'MS'; 
-
-console.log(sobrenome, cidade)
-
-var semDefenir;
-console.log(semDefenir);
-
- var comida
- console.log(comida);
- comida = 'Pizza';
- console.log(comida);
-
- var time ='Vasco';
- time = 'flamengo';
- console.log(time);
+  var nome = ' Matheus'
+  var idade = 28;
+  var possuiFaculdade = true; 
 
 
- var nome ='Matheus'
- var sobrenome = 'Oliveira'
 
- console.log(nome, sobrenome);
+  console.log(nome, idade, possuiFaculdade);
 
- var idade = '29'
- var tipoSanguineo = 'A+'
+   var preco= 25;
+   var totalComprado = 5;
+   var totalPreco = totalComprado * preco;
 
- console.log(idade, tipoSanguineo);
+   console.log(totalPreco);
 
- var comida = 'pizza';
- var semDefenir;
+   var sobrenome = 'Rafael', 
+    cidade = 'Rio';
 
- console.log(comida, semDefenir)
+   console.log(sobrenome, cidade);
 
- var time, 
-    escola, 
-    cidade, 
-    pais, 
-    curso;
+var semDefinir
+console.log(semDefinir)
+
+//console.log(aindaNaoDefine);
+
+// var time = 'vasco'
+//  time = 'flamego'
+// console.log(time)
+
+var nome = 'matheus'
+console.log(nome)
+
+var idade = 29;
+console.log(idade)
+
+var comida = 'pizza';
+console.log(comida)
+
+var comidaFavorita;
