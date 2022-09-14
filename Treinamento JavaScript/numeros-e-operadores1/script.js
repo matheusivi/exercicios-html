9d219087f51ebddbc7c6d80e95c65bd0da7c449b
Matheus William <matheus-willliam@hotmail.com>
2022-09-14 15:50:24 -0300

    js pt 3 operadores

--- Treinamento JavaScript/numeros-e-operadores1/script.js
+++ Treinamento JavaScript/numeros-e-operadores1/script.js
@@ -23,3 +23,22 @@ console.log(divisao)
 
 var testeNaN = 'isso é 100' / 2;
 console.log(testeNaN);
+
+// A ordem importa
+
+var total1 = 20 + 5 * 2;
+var total2 = (20 + 5) * 2;
+var total3 = 20 / 2 * 5;
+var total4 = 10 + 10 * 2 + 20 / 2;
+
+console.log(total4);
+
+//Operadores aritméticos unários
+
+var incremento = 5;
+console.log(incremento++)
+console.log(incremento);
+
+var incremento2 = 5;
+console.log(++incremento2);
+console.log(incremento2);
