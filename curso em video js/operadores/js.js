b6fefa3b28f41f360463638ad1fb7090028c6470
Matheus William <matheus-willliam@hotmail.com>
2022-09-18 16:06:37 -0300

    operadores pt1

---
+++ curso em video js/operadores/js.js
@@ -0,0 +1,31 @@
+/* a ordem de precedência da matemática.
+ ()
+ ** potência
+ * / %
+ * 
+ + -
+
+
+var a = 5 + 3
+var b = a % 5
+var c = 5 * b ** 2
+var d = 10 - a / 2
+var e = 6 * 2 / d 
+var f = b % e + 4 / e
+
+
+> var n = 3
+> n = n + 4     n + = 4
+> n = n - 5     n -=5
+> n = n * 4     n *= 4    
+> n = n / 2     n /= 2
+> n = n ** 2    n **= 2 
+> n = n % 5     n %=5
+
+
+incremento
+
+var x = 5
+x = x + 1;   n += 1; ou x++
+x = x - 1;   n -= 1; ou x--
+*/
