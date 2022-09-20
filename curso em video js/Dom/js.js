be148c4b44a8c3c5c60a6255c9bf128dbbfc09c5
Matheus William <matheus-willliam@hotmail.com>
2022-09-20 14:34:00 -0300

    inicio de dom js

---
+++ curso em video js/Dom/js.js
@@ -0,0 +1,9 @@
+var p1 = window.document.getElementsByTagName('p')[1]
+
+window.document.write('Está escrito assim: ' + p1.innerHTML)
+
+p1.style.color = 'blue';
+
+var corpo = window.document.body
+corpo.style.background = 'black'
+
