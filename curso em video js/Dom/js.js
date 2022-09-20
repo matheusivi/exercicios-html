eb1ca4eb158289b120b6ad7273f89123401650c8
Matheus William <matheus-willliam@hotmail.com>
2022-09-20 14:43:36 -0300

    fim de inicio de dom

--- curso em video js/Dom/js.js
+++ curso em video js/Dom/js.js
@@ -7,3 +7,10 @@ p1.style.color = 'blue';
 var corpo = window.document.body
 corpo.style.background = 'black'
 
+/*var d = window.document.getElementById('msg')
+d.style.background = 'green'
+d.innerText = 'Estou aguardando' Tem essa forma e a forma de baixo para trabalhar, da na mesma, mas esse é mais simplificado
+window.document.getElementById('msg').innerText = 'Olá!'*/
+
+var d = window.document.querySelector('div#msg')
+d.style.background= 'blue'
