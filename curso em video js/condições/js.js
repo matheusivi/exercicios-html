38c9479116b4b73bd362fa14089db26d87424f00
Matheus William <matheus-willliam@hotmail.com>
2022-09-21 17:42:51 -0300

    condição

---
+++ curso em video js/condições/js.js
@@ -0,0 +1,26 @@
+/*var vel =  
+console.log(`A velocidade do seu carro é ${vel}km/h `)
+if (vel > 60) { // condição simples!
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança')*/
+
+/*var país = 'Israel'
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil') {
+    console.log('Você é Brasileiro')
+} else {
+    console.log('Você é Estrangeiro')
+}*/
+
+
+function calcular() {
+   var txtv = window.document.querySelector('input#txtvel');
+   var res = window.document.querySelector('div#res');
+   var vel = Number(txtv.value);
+   res.innerHTML = `<p>Sua velocidade atual é de <strong>${vel}km/h</strong></p>`
+   if (vel > 60){
+    res.innerHTML += `Você está <strong>MULTADO</strong> por excesso de velocidade`
+   }
+   res.innerHTML += `<p>Dirika sempre com cinto de segurança!</p> `
+}
