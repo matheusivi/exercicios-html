707dcd2c71e5204b087d9d7fb8a42924196d6fd0
Matheus William <matheus-willliam@hotmail.com>
2022-09-29 15:50:07 -0300

    dom revisao

--- curso em video js/Dom/js.js
+++ curso em video js/Dom/js.js
@@ -1,7 +1,12 @@
 /* var p1 = window.document.getElementsByTagName('p')[1]
-window.document.write('Está escrito assim: ' + p1.innerText);
-p1.style.color = 'black'
-
 var corpo = window.document.body
-corpo.style.background = 'green' */
+window.document.write('Está escrito assim: ' + p1.innerText);
+p1.style.color = 'blue'
+corpo.style.background = 'black'
+
+var d = window.document.getElementById('msg')
+d.style.background= 'green'
+d.innerText = 'Estou aguardando...' 
+//window.document.getElementById('msg').innerText = 'Olá!'
+*/
 
