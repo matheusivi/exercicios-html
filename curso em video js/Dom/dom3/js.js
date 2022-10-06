7bdbd651436f40275f69e10fe978d21f472d7af1
Matheus William <matheus-willliam@hotmail.com>
2022-10-06 18:57:53 -0300

    evento dom, revisão

--- curso em video js/Dom/dom3/js.js
+++ curso em video js/Dom/dom3/js.js
@@ -1,4 +1,4 @@
-function somar(){
+ function somar(){
     var tn1 = window.document.getElementById('txtn1')
     var tn2 = window.document.querySelector('input#txtn2')
     var res = window.document.getElementById('res')
@@ -6,4 +6,5 @@ function somar(){
     var n2 = Number(tn2.value)
     var s = n1 + n2
     res.innerHTML = `A soma entre ${n1} e ${n2} é igual a <strong>${s}</strong>`
-}
+} 
+
