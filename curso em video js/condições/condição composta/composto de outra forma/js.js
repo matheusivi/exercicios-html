07b5f8b88381d7bb0597810addd9700ebd9a6c85
Matheus William <matheus-willliam@hotmail.com>
2022-10-10 17:06:51 -0300

    outro exemplo de condição

---
+++ curso em video js/condições/condição composta/composto de outra forma/js.js
@@ -0,0 +1,12 @@
+
+
+function calcular(){
+    var txtv = window.document.getElementById('txtvel')
+    var res = window.document.querySelector('div#res')
+    var vel = Number(txtv.value)
+    res.innerHTML = `<p>Sua velocidade atual é <strong>${vel}km/h</strong></p>`
+    if (vel > 60){
+        res.innerHTML = `<p>Você está <strong>multado</strong> por excesso de velocidade</p>`
+    }
+    res.innerHTML += `<p>Dirija sempre com cinto de segurança</p>`
+}
