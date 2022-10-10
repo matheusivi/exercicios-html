96f9d81af83c80ea8c5744ec8840a40362554f3a
Matheus William <matheus-willliam@hotmail.com>
2022-10-10 15:15:16 -0300

    revisao

--- curso em video js/Dom/js.js
+++ curso em video js/Dom/js.js
@@ -1,12 +1,5 @@
-/* var p1 = window.document.getElementsByTagName('p')[1]
 var corpo = window.document.body
-window.document.write('Está escrito assim: ' + p1.innerText);
+var p1 = window.document.getElementsByTagName('p')[0]
+window.document.write('Está escrito assim: ' + p1.innerText)
 p1.style.color = 'blue'
 corpo.style.background = 'black'
-
-var d = window.document.getElementById('msg')
-d.style.background= 'green'
-d.innerText = 'Estou aguardando...' 
-//window.document.getElementById('msg').innerText = 'Olá!'
-*/
-
