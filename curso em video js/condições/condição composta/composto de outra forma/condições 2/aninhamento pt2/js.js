89e96bd8f3bc31547bd0cf712efcbc3855a0f77a
Matheus William <matheus-willliam@hotmail.com>
2022-10-11 14:49:47 -0300

    condição composta else if prt2

---
+++ curso em video js/condições/condição composta/composto de outra forma/condições 2/aninhamento pt2/js.js
@@ -0,0 +1,13 @@
+var agora = new Date()
+var horas = agora.getHours()
+console.log (`Agora são exatamente ${horas} horas`)
+if ( horas > 6 && horas <= 12.59){
+    console.log('Bom Dia!')
+} else if (horas > 12.01 && horas <= 18) {
+    console.log('Boa tarde!')
+
+} else if (horas > 18.1 && horas < 23.59) {
+    console.log('Boa noite!')
+} else {
+    console.log('Boa Madrugada!')
+}
