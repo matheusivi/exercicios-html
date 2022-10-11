c456b95e61990617ab7ae271bb5d47717841e662
Matheus William <matheus-willliam@hotmail.com>
2022-10-11 18:15:32 -0300

    treinamento condição

--- curso em video js/condições/condição composta/composto de outra forma/js.js
+++ curso em video js/condições/condição composta/composto de outra forma/js.js
@@ -12,3 +12,14 @@ function calcular(){
 }*/
 
 
+function calcular(){
+    var txvel = window.document.getElementById('tn1')
+    var res = window.document.getElementById('res')
+    var t1 = Number(txvel.value)
+    res.innerHTML = `<p>A sua velocidade é <strong>${t1}Km/h</strong></p>`
+    if ( t1 > 70){
+        res.innerHTML = ('<p>Você está <strong>MULTADO</strong> por excesso de velocidade.</p>')
+
+    }
+    res.innerHTML += `<p>Dirija com cuidado, use sinto de segurança.</p>`
+}
