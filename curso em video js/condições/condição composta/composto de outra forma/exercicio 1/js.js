46bf37cb0d6656106ea043385489f6d81dadeb61
Matheus William <matheus-willliam@hotmail.com>
2022-10-14 15:28:10 -0300

    exercicio prt2

--- curso em video js/condições/condição composta/composto de outra forma/exercicio 1/js.js
+++ curso em video js/condições/condição composta/composto de outra forma/exercicio 1/js.js
@@ -3,17 +3,20 @@ var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('img')
   
 var data = new Date()
-//var hora = data.getHours()
-var hora = 13
+var hora = data.getHours()
+
 msg.innerHTML = `Agora são ${hora} horas.`
 if (hora >= 6 && hora < 12){
    
     img.src='fotos/manhã.jpg'
+    document.body.style.background = '#e2cd9f'
 }else if (hora >= 12 && hora < 18){
     
     img.src = 'fotos/tarde.jpg'
+    document.body.style.background = '#b9846f'
 }else {
     
     img.src= 'fotos/Noite.jpg'
+    document.body.style.background = '#515154'
 }
 }
