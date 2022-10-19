1b69798fd9114b4620e6529a4173f92efa5ff08a
Matheus William <matheus-willliam@hotmail.com>
2022-10-19 14:16:41 -0300

    revisao geral

--- curso em video js/Repetições/Repetição/repetição de variavel de controle/exercicios 1/js.js
+++ curso em video js/Repetições/Repetição/repetição de variavel de controle/exercicios 1/js.js
@@ -6,28 +6,30 @@ function contar(){
 
     if( ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
         window.alert('[ERRO] Faltam dados!')
+
     } else {
-        res.innerHTML = 'Contando: <br>'
-       let i = Number(ini.value)
-       let f = Number(fim.value)
-       let p = Number(passo.value)
-       if( p <= 0){
-        window.alert('Passo inválido!Considerando PASSO 1')
-        p = 1
-       }
-       if ( i < f){
-       for( let c = i; c <= f; c += p){
-                res.innerHTML += `${c} \u{1f449}`
-            }
+        res.innerHTML = 'Contando:' 
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+      if( i < f){
 
-         
+        for ( c = i; c <= f; c += p){
+                res.innerHTML += `${c}`
+            }
+      
         }else {
-            for (let c = i; c >= f ; c -= p){
-                res.innerHTML += `${c} \u{1f449}`
+            for ( c = i; c >= f; c -= p){
+                res.innerHTML += `${c}`
             }
-
         }
-        res.innerHTML += `\u{1f3c1}`             
-          
+    
+    
     }
 }
+
+
+
+
+
+
