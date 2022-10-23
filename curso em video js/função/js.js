404f94a75d18a240c0089251aa281e48b8841cf3
Matheus William <matheus-willliam@hotmail.com>
2022-10-23 18:46:51 -0300

    revendo function

--- curso em video js/função/js.js
+++ curso em video js/função/js.js
@@ -1,16 +1,35 @@
 /*
-function parimpar(n){
-    if(n % 2 == 0){
-        return 'par!'
+function parimp(n){
+    if (n%2 == 0){
+        return 'Par!'
     }else {
-        return 'impar!'
+        return 'Impar!'
     }
 }
-console.log(parimpar(223));
-*/
 
+console.log(parimp(223))
+*/
+/*
 function soma(n1=0,n2=0){
     return n1 + n2
 }
 
-console.log(soma( 3 ,4))
+console.log(soma(2,5))
+*/
+/*
+let v = function(x){
+    return x*2
+}
+
+console.log(v(5))
+*/
+
+function fatorial(n){
+    let fat = 1 
+    for (let c = n; c > 1; c--){
+        fat *= c
+    }
+    return fat
+}
+
+console.log(fatorial(5))
