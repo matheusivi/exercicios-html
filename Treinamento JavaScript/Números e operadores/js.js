3e6b3ac0b280f51312afd8ef51b1f22574a1b428
Matheus William <matheus-willliam@hotmail.com>
2022-10-24 16:35:21 -0300

    numeros e dados

---
+++ Treinamento JavaScript/Números e operadores/js.js
@@ -0,0 +1,75 @@
+/*
+var idade = 2e-2
+var idade = 2e10
+var idade = 2.5
+console.log(idade)
+
+var soma = 100 + 50
+var subtracao = 100 - 50
+var multiplicacao = 100 * 2
+var divisao = 100 / 2
+var expoente = 2 ** 4
+var modulo = 14 % 5
+
+console.log(modulo)
+
+var soma = '100' + 50 // 10050
+var subtração = '100' - 50 // 50
+var multiplicacao = '100' * '2' // 200
+var divisao = 'comprei 10' / 2 // NaN 
+console.log(isNaN(divisao)) 
+*/
+
+var total1 = 20 + 5 * 2 // 30
+var total2 = (20 + 5) * 2 // 50
+var total3 = 20 / 2 * 5 // 50
+var total4 = 10 + 10 * 2 + 20 / 2 // 40
+
+var incremento = 5;
+console.log(incremento ++) // 5
+console.log(incremento) // 6
+
+var incremento2 = 5
+console.log(++incremento2) //6
+console.log(incremento2) //6
+
+// a mesma forma acontece com o descremento exemplo (--x)
+
+var frase = 'Isso é um teste';
++frase; //NaN;
+-frase; //NaN;
+
+var idade = '29'
++idade; // 29 (número)
+-idade; // -29 (número)
+console.log(+idade + 5); // 33
+
+var possuiFaculdade = true; 
+console.log(+possuiFaculdade); // 1
+
+
+
+var total = 10 + 5 * 2 / 2 + 20;
+console.log(total);
+
+var idade = 'tenho 29 anos'
+var tempo = '10 mm'
+
+console.log(+idade)
+console.log(-tempo)
+
+var numero = '200';
+var valor = 50;
+console.log(+numero + valor);
+
+let incrementar = 5;
+console.log(incrementar++);
+console.log(incrementar);
+
+var numero = '80' / 2;
+var unidade = 'kg';
+var peso = numero + unidade;
+
+
+
+
