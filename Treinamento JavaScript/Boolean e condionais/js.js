f82674641bc58153ed1046c8188cd261f020253f
Matheus William <matheus-willliam@hotmail.com>
2022-10-26 16:57:19 -0300

    operadores lógicos

---
+++ Treinamento JavaScript/Boolean e condionais/js.js
@@ -0,0 +1,52 @@
+
+/*
+var x;
+var possuiGraduacao = true;
+var possuiDoutorado = true;
+
+if(possuiGraduacao){
+    console.log('é verdadeiro');
+    var x = 10;
+}else if(possuiDoutorado){
+    console.log('É falso')
+}
+
+console.log(x);
+
+
+
+var nome = ''
+
+if(nome){
+    console.log(nome)
+} else{
+    console.log('Nome não existe')
+}
+//uma string sem nada, ela é considerada falsa.
+/*
+false
+if(false)/ if(0)/ if(NaN)/ if(null)/ if(undefined)/ if(''). Todas essas vão dar false.
+
+
+
+Operadores lógicos
+if(!true)// false
+if(!1) // false
+if(!'')// true
+if(!undefined)// true
+if(!!'  ')// true
+if(!! '')// false
+
+Operadores de comparação
+O == faz uma comparação não tão estrita e o === faz uma comparação estrita, ou seja, o tipo de dado deve ser o mesmo quando usamos ===.
+
+10 =='10' // true
+10 == 10 // true
+10 === '10' // false
+10 === 10 // true
+10 != 15 // true
+10 != '10' // false
+10 !== '10' //
+*/
+
+
