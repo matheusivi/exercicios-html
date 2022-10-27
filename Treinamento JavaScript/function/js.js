2625c48055682d12a97780be1c8153d2fac342f1
Matheus William <matheus-willliam@hotmail.com>
2022-10-27 17:08:52 -0300

    função pt1

---
+++ Treinamento JavaScript/function/js.js
@@ -0,0 +1,44 @@
+/*
+function areaQuadrado(lado){
+    return lado * lado;
+}
+
+console.log(areaQuadrado(25));
+
+function pi(){
+    return 3.14
+}
+var total = 5 * pi();
+console.log(total)
+
+
+//Ao criar uma função, você pode definir parâmetros.
+// Ao executar uma função. Você pode passar argumentos.
+// peso e altura são os parâmentros
+function imc(peso, altura){
+    const imc = peso / (altura ** 2);
+    return imc;
+}
+
+console.log(imc(80, 1.80)) // 80 e 1.80 são os argumentos
+imc( 60, 1.70 ) // 60 e 1.70 são os arguementos
+
+function corFavorita(cor){
+    if(cor === 'azul'){
+        return 'Você gosta do céu';
+    } else if(cor === 'verde'){
+        return 'Você gosta de mato';
+    }else {
+        return 'você não gosta de nada';
+    }
+}
+
+console.log(corFavorita('azul'))
+
+
+addEventListener('click', function(){
+    console.log('oi')
+})
+
+*/
+
