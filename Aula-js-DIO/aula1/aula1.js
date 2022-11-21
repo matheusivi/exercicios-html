78fdb6089f335f768d3ba538f139a9184eca8ea6
Matheus William <matheus-willliam@hotmail.com>
2022-11-21 18:38:57 -0300

    estudo de js inicio

---
+++ Aula-js-DIO/aula1/aula1.js
@@ -0,0 +1,9 @@
+
+
+let combustivel = 5.30;
+let carro = 20;
+let distancia = 280;
+
+let resolucao = 5.20 * 20 / 280
+
+console.log(resolucao)
--- curso em video js/Dom/js.js
+++
@@ -1,5 +0,0 @@
-var corpo = window.document.body
-var p1 = window.document.getElementsByTagName('p')[0]
-window.document.write('Está escrito assim: ' + p1.innerText)
-p1.style.color = 'blue'
-corpo.style.background = 'black'
