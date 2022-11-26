c065672f6352debb21c7fde8df2684190d796be0
Matheus William <matheus-willliam@hotmail.com>
2022-11-26 15:40:20 -0300

    js eu vou conseguir

--- js-Dio/Funções/exercicio1.js
+++ js-Dio/Funções/exercicio1.js
@@ -1,11 +1,39 @@
-function calcularImc(peso, altura){
-
-    return peso/ Math.pow(altura, 2);
+/*
+function escrevaNome(nome){
+   return 'Meu nome é ' + nome;
 }
 
-const peso = 75;
-const altura = 1.75;
-const imc = calcularImc(peso, altura)
-console.log(imc);
- 
-function classificarImc()
+function verificarIdade(idade){
+    if(idade >= 18){
+        console.log (escrevaNome ('Matheus') + ' ,Maior de Idade')
+    }else{
+        console.log('Menor de idade')
+    }    
+}
+
+verificarIdade(18)
+*/
+
+
+function aplicarDesconto(valor, desconto){
+    return (valor - (valor * desconto/100))
+}
+
+function aplicarJuros(valor, juros){
+    return (valor + (valor * juros/100))
+}
+
+const precoEtiqueta = 100;
+const formaDePagamento = 4;
+
+
+
+if(formaDePagamento === 1){
+    console.log(aplicarDesconto(precoEtiqueta, 10))
+}else if (formaDePagamento === 2){
+    console.log(aplicarDesconto(precoEtiqueta, 15))
+}else if(formaDePagamento === 3){
+    console.log(precoEtiqueta)
+}else {
+    console.log(aplicarJuros(precoEtiqueta, 10))
+}
