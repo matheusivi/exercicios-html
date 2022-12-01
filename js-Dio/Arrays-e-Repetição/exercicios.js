77aa3b7c1ee2e50ffd23f649328dee5773ad492e
Matheus William <matheus-willliam@hotmail.com>
2022-12-01 20:02:50 -0300

    questao add

--- js-Dio/Arrays-e-Repetição/exercicios.js
+++ js-Dio/Arrays-e-Repetição/exercicios.js
@@ -5,7 +5,7 @@ for (let i = 1; i <= 10; i++) {
   
     console.log(i * divisor); 
 }
-*/
+
 const mes = [0, 1, 2, 3, 4, 5, 6, 7, 8 , 9, 10, 11 , 12];
 
 for (let i = 0; i < mes.length; i++) {
@@ -14,8 +14,12 @@ for (let i = 0; i < mes.length; i++) {
     if(numero % 2 === 0 ){
         console.log(numero)
     }
+}
+*/
+numerosPares= []
 
-    
-    
+for (let i = 0; i < 10; i = i + 2) {
+    numerosPares.push(i);
 }
 
+console.log(numerosPares)
