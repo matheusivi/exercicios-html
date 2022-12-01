8ca05afb10410836e752273e200014e61037a70e
Matheus William <matheus-willliam@hotmail.com>
2022-12-01 19:55:47 -0300

    exercicios

---
+++ js-Dio/Arrays-e-Repetição/exercicios.js
@@ -0,0 +1,21 @@
+/*
+ const divisor = 7
+for (let i = 1; i <= 10; i++) {
+   
+  
+    console.log(i * divisor); 
+}
+*/
+const mes = [0, 1, 2, 3, 4, 5, 6, 7, 8 , 9, 10, 11 , 12];
+
+for (let i = 0; i < mes.length; i++) {
+    const numero = mes[i];
+
+    if(numero % 2 === 0 ){
+        console.log(numero)
+    }
+
+    
+    
+}
+
