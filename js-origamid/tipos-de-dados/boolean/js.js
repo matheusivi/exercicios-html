a874dc7f3c38e912d284352b7545b511b5f723fe
Matheus William <matheus-willliam@hotmail.com>
2023-01-10 20:03:08 -0300

    aula de boolean

---
+++ js-origamid/tipos-de-dados/boolean/js.js
@@ -0,0 +1,33 @@
+/*
+var possuiGraduacao = false;
+
+if(possuiGraduacao){
+    console.log('É verdadeiro')
+    var x = 10;
+}else{
+    console.log('É falso')
+    var x = 10;
+}
+
+console.log(x)
+
+
+var possuiGraducao = false;
+var possuiGraducao = true;
+
+if(possuiGraducao){
+    console.log('É verdadeiro');
+}else if(possuiDoutorado){
+    console.log('Possui Doutorado');
+}else {
+    console.log('Não possui nada');
+}
+*/
+
+var nome = '10kg' / 10;
+
+if(nome) {
+    console.log(nome);
+}else {
+    console.log('Nome não existe')
+}
