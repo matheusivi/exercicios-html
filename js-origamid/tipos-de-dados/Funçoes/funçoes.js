7fc68965366d3c263c07ea94c2f50fce7127bdd8
Matheus William <matheus-willliam@hotmail.com>
2023-01-19 21:03:54 -0300

    revisao função

---
+++ js-origamid/tipos-de-dados/Funçoes/funçoes.js
@@ -0,0 +1,43 @@
+/*
+function areaQuadrado(lado){
+    return lado * lado
+}
+
+console.log(areaQuadrado(5))
+
+function pi(){
+    return 3.14;
+}
+
+var total = 5 * pi();
+console.log(total)
+
+
+function imc(peso, altura){
+    var imc = peso / ( altura * altura)
+    return imc
+}
+
+console.log(imc(105, 1.87))
+
+
+function corFavorita(cor){
+    if(cor === 'azul'){
+        return 'Eu gosto do céu'
+    }else if(cor === 'verde'){
+        return 'Eu gosto de floresta'
+    }else {
+        return 'eu não gosto de cores'
+    }
+}
+
+console.log(corFavorita('verde'))
+
+
+addEventListener('click', function(){console.log('oi')})*/
+
+function mostreConsole(){
+    console.log('oi')
+}
+
+addEventListener('click', mostreConsole)
