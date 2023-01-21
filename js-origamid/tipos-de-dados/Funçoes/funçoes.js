0102ee09c8de9cc7517432803f4120f0fe21290c
Matheus William <matheus-willliam@hotmail.com>
2023-01-21 19:40:50 -0300

    aula de function

--- js-origamid/tipos-de-dados/Funçoes/funçoes.js
+++ js-origamid/tipos-de-dados/Funçoes/funçoes.js
@@ -34,10 +34,55 @@ function corFavorita(cor){
 console.log(corFavorita('verde'))
 
 
-addEventListener('click', function(){console.log('oi')})*/
+addEventListener('click', function(){console.log('oi')})
 
 function mostreConsole(){
     console.log('oi')
 }
 
 addEventListener('click', mostreConsole)
+
+
+function imc2(peso, altura){
+    const imc = peso / (altura ** 2);
+    console.log(imc);
+}
+
+imc2(20,1.8);
+
+console.log(imc2(2000,1.8));
+
+
+function terceiraIdade(idade){
+    if( typeof idade !== 'number'){
+        return 'Por favor, preencha com Número!'
+    }else if (idade >= 60){
+        return true;
+    }else {
+        return false;
+    }
+}
+
+console.log(terceiraIdade('sessenta'));
+
+var totalPaises = 193;
+function faltaVisitar(paisesVisitados){
+   
+    return `Falta visitar ${totalPaises - paisesVisitados}`
+}
+console.log(faltaVisitar(20));
+*/
+var profissao = 'Farmáceutico';
+
+function dados(){
+    var nome = 'Matheus';
+    var idade = 28;
+    function outrosDados() {
+        var endereco = 'Mato Grosso do Sul';
+        var idade = 29;
+        return `${nome}, ${idade}, ${endereco}, ${profissao}`;
+    }
+    return outrosDados();
+}
+
+console.log(dados())
