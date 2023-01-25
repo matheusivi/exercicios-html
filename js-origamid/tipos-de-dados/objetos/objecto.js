dc860ab7760deab508b49c332a28304b9a18ecc8
Matheus William <matheus-willliam@hotmail.com>
2023-01-25 20:59:48 -0300

    objetos

---
+++ js-origamid/tipos-de-dados/objetos/objecto.js
@@ -0,0 +1,24 @@
+/*
+var pessoa = {
+    nome: "Matheus",
+    idade: 29,
+
+}
+console.log(pessoa);
+
+
+var quadrado = {
+    lados: 4,
+    area: function(lado){ // ou pode juntar area(lado)
+        return lado * lado;
+    },
+    perimetro: function(lado){ // mesma forma perimetro(lado), funciona da mesma forma a função.
+        return this.lados * lado; 
+    }
+}
+
+
+console.log(quadrado.area(5));
+console.log(quadrado.perimetro(5));
+*/
+
