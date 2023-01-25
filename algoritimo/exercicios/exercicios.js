ee15d39b33acec8146776c8a319bb5f5238bf226
Matheus William <matheus-willliam@hotmail.com>
2023-01-25 19:49:05 -0300

    exercicios algoritmos

---
+++ algoritimo/exercicios/exercicios.js
@@ -0,0 +1,53 @@
+/*
+function area(comprimento, largura){
+    var area = comprimento  * largura
+    return area 
+}
+
+var imobilis = console.log(area(10, 25))
+
+
+
+function cavalos(quantAnimal){
+    var animais = quantAnimal * 4;
+    return animais;
+}
+
+var quantferraduras = console.log(cavalos(10));
+
+
+var paesFranceses = 0.12;
+var broa = 1.50;
+
+function vendas(fVendas, bVendas){
+    var totalVendaF = paesFranceses * fVendas;
+    var totalVendaB = broa * bVendas;
+    var soma = totalVendaF + totalVendaB;
+    var porcentagem = (soma * 0.10); 
+    var resultadoFinal = `A soma total das vendas do dia é ${soma}, e os 10% arrecadado para o depósito é de ${porcentagem}.`;
+    return resultadoFinal;
+}
+
+var Deposito = console.log(vendas(80, 58));
+
+
+function tempoDeVida(nome, idade){
+    var diasVivido = idade * 365;
+    var final = `${nome}, você já viveu um total de ${diasVivido};`
+    return final;
+}
+
+var resultado = console.log(tempoDeVida('Matheus', 30));
+
+
+
+
+function Abastecimento(gasolina, pagamento){
+    var preco = Math.floor(gasolina * pagamento);
+    var litros = Math.floor(pagamento / gasolina);
+    var final = `O total pago pelo litro de gasolina foi de ${preco} R$, e a quantidade colocada foi de ${litros} litros. `;
+    return final;
+}
+
+var resultado = console.log(Abastecimento(4.98, 100));
+*/
