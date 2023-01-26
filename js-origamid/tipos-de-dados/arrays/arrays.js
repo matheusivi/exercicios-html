efb2129afb03941474e91e86c02730b7ba3e99d1
Matheus William <matheus-willliam@hotmail.com>
2023-01-26 20:38:46 -0300

    estudo e exercicios

---
+++ js-origamid/tipos-de-dados/arrays/arrays.js
@@ -0,0 +1,9 @@
+var videoGame = ['Switch', 'Ps4', 'XBOX'];
+
+var ultimoItem = videoGame.pop();
+
+videoGame.push('3DS');
+
+for (var numero = 0; numero <= 10; numero++ ){
+    console.log(numero)
+}
--- js-origamid/tipos-de-dados/objetos/objecto.js
+++ js-origamid/tipos-de-dados/objetos/objecto.js
@@ -20,5 +20,78 @@ var quadrado = {
 
 console.log(quadrado.area(5));
 console.log(quadrado.perimetro(5));
+
+var menu = {
+    width: 800,
+    height: 50,
+    backgroundColor: '#84e',
+};
+
+menu.backgroundColor = '#000';
+console.log(menu.backgroundColor);
+
+var height = 120;
+var menu = {
+    width: 800,
+    height: 50,
+    backgroundColor: "#84e",
+    metadeHeight(){
+        return  height / 2;
+    }
+}
+
+
+var dados = {
+    nome: 'Matheus',
+    sobrenome: 'Oliveira',
+    idade: 29,
+    cidade: "Nova Andradina",
+    profissão: 'Farmacêutico',
+    outrosDados(){
+        var nomeCompleto = 'Matheus William de OLiveira';
+        return nomeCompleto
+    }
+}
+
+dados.nomeCompleto = function(){
+    return `${this.nome} ${this.sobrenome}`
+}
+
+var carro = {
+    preco: 1000,
+    portas: 4,
+    marca: 'Audi',
+}
+
+carro.marca = 'ferrari';
+carro.portas = 2;
+
+
+var animal = {
+    tipo: 'Cachorro',
+    cor: 'Preto',
+    raca: 'Labrador',
+    idade: 10,
+    latir(pessoa){
+        if(pessoa === 'homem'){
+            return 'Latir';
+        }else{
+            return 'Nada';
+        }
+    }
+}
 */
 
+//TUDO É OBJETOS!!!
+
+console.log(11.8.toFixed());
+console.log(11.3.toFixed());
+
+
+function areaQuadrado(lado){
+    return lado * lado;
+}
+
+console.log(areaQuadrado.toString());// envolveu a função em uma string.
+
+console.log(areaQuadrado.length) // me mostra o quando tem de parametros na função.
