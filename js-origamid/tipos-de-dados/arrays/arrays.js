cea62cc95ca2bca6b95c603eeeaf9ed8bf407c52
Matheus William <matheus-willliam@hotmail.com>
2023-01-29 16:51:12 -0300

    array e loop

--- js-origamid/tipos-de-dados/arrays/arrays.js
+++ js-origamid/tipos-de-dados/arrays/arrays.js
@@ -1,9 +1,41 @@
-var videoGame = ['Switch', 'Ps4', 'XBOX'];
+var videoGame = ['Switch', 'Ps4', 'XBOX', '3DS'];
 
-var ultimoItem = videoGame.pop();
+/*var ultimoItem = videoGame.pop();
 
 videoGame.push('3DS');
 
 for (var numero = 0; numero <= 10; numero++ ){
     console.log(numero)
+}
+
+
+for (var item = 0; item < 4; item ++){
+        console.log(videoGame[item]);
+        if(videoGame[item] === 'Ps4'){
+            break;
+        }
+}
+
+
+var frutas = ['banana', 'pera', 'maçã', 'uva', 'melancia'];
+
+frutas.forEach(function(fruta, index){
+        console.log(fruta, index);
+});
+*/
+
+var copa = [1959, 1962, 1970, 1994, 2002 ];
+
+
+for (let i = 0 ; i < copa.length; i++) {
+     console.log(` O Brasil ganhou a copa de ${copa[i]}`);
+}
+
+var frutas = ['banana', 'maçã', 'pera', 'uva', 'melância'];
+
+for (var i = 0; i < frutas.length; i++){
+    console.log(frutas[i]);
+    if(frutas[i] === 'pera'){
+        break;
+    }
 }
