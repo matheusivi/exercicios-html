1b8a925802826d00932e437935ac94f703e47607
Matheus William <matheus-willliam@hotmail.com>
2023-01-30 20:50:28 -0300

    aulas e exercicios

---
+++ js-origamid/tipos-de-dados/escopo/escopo.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+{
+    var mes = 'Dezembro';
+    console.log(mes);
+}
+console.log(mes);
+
+const  semana = 'Sexta';
+
+console.log(semana);
+
+const data = {
+    ano: 2018,
+    mes:'Dezembro'
+}
+
+console.log(data.ano = 2020);
+console.log(data.mes = 'Janeiro')
+console.log(data.dia = 29)
+console.log(data)
+
+
+{
+    var cor = 'Preto';
+    const marca= 'Fiat';
+    let portas = 4;
+}
+//console.log(var, marca, portas)
