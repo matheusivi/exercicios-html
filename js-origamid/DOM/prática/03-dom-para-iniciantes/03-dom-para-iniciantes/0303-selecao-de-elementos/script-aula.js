b0425e36ae098486fbd5dde79ab056c06be9fbd4
Matheus William <matheus-willliam@hotmail.com>
2023-02-11 18:24:17 -0300

    treinamento

--- js-origamid/DOM/prática/03-dom-para-iniciantes/03-dom-para-iniciantes/0303-selecao-de-elementos/script-aula.js
+++ js-origamid/DOM/prática/03-dom-para-iniciantes/03-dom-para-iniciantes/0303-selecao-de-elementos/script-aula.js
@@ -1,35 +1,25 @@
-const animais = document.getElementById('animais');
-console.log(animais);
+//const animais = document.getElementById('animais');
+//console.log(animais.innerHTML);  
 
-const gridSection = document.getElementsByClassName('grid-section');
-console.log(gridSection[1])
+const gridSelector = document.getElementsByClassName('grid-section');
+console.log(gridSelector[0]);
 
 const primeiraLi = document.querySelector('li');
-console.log(primeiraLi)
+console.timeLog(primeiraLi);
 
-const primeiraUl = document.querySelector('ul');
-console.log(primeiraUl)
+const primeiroUl = document.querySelector('ul');
+console.log(primeiroUl);
 
 const linkInterno = document.querySelector('[href^="#"]');
 console.log(linkInterno);
 
-const animaisImg = document.querySelectorAll('.animais img');
-console.log(animaisImg[0]);
+const animaisIMg = document.querySelectorAll('.animais img');
+console.log(animaisIMg[0]);
 
-const gridSectionHTML = document.getElementsByClassName('grid-section');
+const gridSectionHtml = document.getElementsByClassName('grid-section');
 const gridSectionNode = document.querySelectorAll('.grid-section');
 
-primeiraUl.classList.add('grid-section');
+primeiroUl.classList.add('grid-section');
+console.log(gridSectionHtml);
+console.log(gridSectionNode);
 
-// console.log(gridSectionHTML[0]);
-// console.log(gridSectionNode[0]);
-
-gridSectionNode.forEach(function(item, index){
-  console.log(item);
-});
-
-const arrayGrid = Array.from(gridSectionHTML);
-
-arrayGrid.forEach(function(item) {
-  console.log(item);
-})
