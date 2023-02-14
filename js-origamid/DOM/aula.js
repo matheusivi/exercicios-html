41e3ec9aa5f65f21c6c2309982f45a6185e08c01
Matheus William <matheus-willliam@hotmail.com>
2023-02-14 20:37:24 -0300

    aula

---
+++ js-origamid/DOM/aula.js
@@ -0,0 +1,21 @@
+//const animais = document.getElementById('animais');
+//console.log(animais.innerHTML);
+
+//const gridSection = document.getElementsByClassName('grid-section');
+//console.log(gridSection);
+
+const primeiraUl = document.querySelector('ul');
+//console.log(primeiraUl);
+
+//const linkInterno = document.querySelector('[href^="#"]');
+//console.log(linkInterno);
+//const animaisImg = document.querySelectorAll('.animais img');
+//console.log(animaisImg);
+
+const gridSectionHtml = document.getElementsByClassName('grid-section');
+const gridSectionNode = document.querySelectorAll('grid-section');
+
+primeiraUl.classList.add('grid-section');
+
+console.log(gridSectionHtml);
+console.log(gridSectionNode);
