e9202a4a6fe5d0fbc6a9fa6a2f07e2ecfc54c883
Matheus William <matheus-willliam@hotmail.com>
2023-02-21 12:03:20 -0300

    estudo

---
+++ js-origamid/DOM/Selecao-Elementos/aula.js
@@ -0,0 +1,19 @@
+const animais = document.getElementById('animais');
+console.log(animais);
+
+const gridSection = document.getElementsByClassName('grid-section');
+
+console.log(gridSection[2]);
+
+const primeiraLi = document.querySelector('li');
+console.log(primeiraLi);
+
+const primeiraUl = document.querySelector('ul');
+console.log(primeiraUl);
+
+
+const link = document.querySelector('[href^="#"]');
+console.log(link);
+
+const animaisImg = document.querySelectorAll('.animais img');
+console.log(animaisImg[2])
