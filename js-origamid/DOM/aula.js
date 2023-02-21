0a09f6ed032a6631a8fc6cb9b8786dde05f6e022
Matheus William <matheus-willliam@hotmail.com>
2023-02-20 22:44:48 -0300

    aulas

--- js-origamid/DOM/aula.js
+++ js-origamid/DOM/aula.js
@@ -99,7 +99,101 @@ tituloArray.forEach((item, index) => {
 
         imgs.forEach(() => console.log(i++)
     })
+
+
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach(function(item, index, array){
+    //console.log(item, index, array)
+})
+
+const titulos = document.getElementsByClassName('titulo');
+const titulosArray = Array.from(titulos);
+
+console.log(titulos);
+console.log(titulosArray);
+
+titulosArray.forEach(function(item, index, array){
+    console.log(item, index)
+})
+
+
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach((item) =>{
+    console.log(item);
+})
+
+
+const imgs = document.querySelectorAll('img');
+
+let i = 0;
+imgs.forEach(function(){
+    console.log(i++)
+});
+
+// ou versão mais curta e que vai ser utilizada.
+
+imgs.forEach(() => console.log(i++));
+
+
+const paragrafo = document.querySelectorAll('p');
+console.log(paragrafo);
+
+paragrafo.forEach((item)=>{
+    console.log(item)
+});
+
+
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach((item, index) => {
+    console.log(item, index)
+})
+
+let i = 0;
+imgs.forEach(() => {
+    console.log(i++)
+})
+
+imgs.forEach(() => i++);
+console.log(i);
 */
 
-const paragrafos = document.querySelectorAll('p');
-console.log(paragrafos[paragrafos.length - 1])
+const menu = document.querySelector('.menu');
+
+menu.classList.add('ativo', 'teste');
+menu.classList.remove('azul');
+menu.classList.toggle('azul')
+
+
+if(menu.classList.contains('azul')){
+    menu.classList.add('possui-azul')
+}else {
+    menu.classList.add('Não-possui-azul');
+}
+
+menu.className += ' vermelho'
+//console.log(menu.classList)
+
+const animais = document.querySelector('.animais');
+console.log(animais.attributes);
+
+const img = document.querySelector('img');
+//console.log(img.getAttribute('src'))
+
+const srcImg = img.getAttribute('alt')
+console.log(srcImg)
+
+img.setAttribute('alt', 'é uma raposa');
+
+const possuiAlt = img.hasAttribute('alt');
+console.log(possuiAlt);
+
+const carro = {
+    portas: 4;
+    andar: function(km) {
+        console.log(`Andou ${}`)
+    }
+}
+
