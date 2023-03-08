ccc8b4a5f268907ce8bed4f0fdbb133bd40c6448
Matheus William <matheus-willliam@hotmail.com>
2023-03-08 18:35:19 -0300

    aulinha

---
+++ js-origamid/DOM/Selecao-Elementos copy/aula.js
@@ -0,0 +1,40 @@
+/*
+const imgs = document.querySelectorAll('img');
+
+
+imgs.forEach(function(item,index,array){
+    console.log(index,item);
+});
+
+const titulos = document.getElementsByClassName('titulo');
+const titulosArray= Array.from(titulos);
+
+titulosArray.forEach(function(item){
+    console.log(item)
+})
+
+
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach((item,index)=>{
+    console.log(item, index)
+})
+
+
+const imgs = document.querySelectorAll('img');
+
+let i = 0
+imgs.forEach(function(){ console.log(i++)});// metodo tradicional
+imgs.forEach(() => console.log(i++));// Novo método 
+*/
+
+const paragrafo = document.querySelectorAll('p');
+
+//paragrafo.forEach(() => {console.log(paragrafo)});
+
+//paragrafo.forEach((item) => {console.log(item.innerText)})
+
+
+
+
+
