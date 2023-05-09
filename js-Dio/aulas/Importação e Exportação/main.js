9b54b20e0e968f888ff8a867aa2d8c0351b4dc36
Matheus William <matheus-willliam@hotmail.com>
2023-05-08 22:46:14 -0300

    treinamento

--- js-Dio/aulas/Importação e Exportação/main.js
+++
@@ -1,8 +0,0 @@
-const {gets, print} = require('./js').default.
-
-const numerosSortiados = {};
-
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
-}
--- js-Dio/aulas/desafio/desafio_1/exercicio 1/js.js
+++
@@ -1,9 +0,0 @@
-const media = [5.5];
-
-if( media < 5){
-    console.log('Reprovado')
-}else if (media >= 5 && media < 7 ){
-    console.log ('Recuperação')
-}else{
-    console.log('Aprovado')
-}
